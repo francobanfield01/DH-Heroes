ea8a68df38b0ea5a4ea13d34b22020e362a279cd
francobanfield01 <franco_banfield@hotmail.com>
2021-09-27 16:28:13 -0300

    Modificacion rutas y agregue breve descripcion de los heroes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('public/img'));
+app.use(express.static('public'));
 
 app.listen(3030, () => console.log("The server is running"));
 
